f69245e915076d15f63164b611f3ab38b4021308
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-05-23 23:12:11 -0700

    More obvious blinking

--- LSM9DS0.cpp
+++ LSM9DS0.cpp
@@ -8,6 +8,22 @@ LSM9DS0::LSM9DS0() {
 	delay(1000);
 	digitalWrite(LED_BUILTIN, LOW);
 	delay(1000);
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
 	Wire.begin();
 
 	pinMode(LED_BUILTIN, OUTPUT);
