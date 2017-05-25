8aacf6ef19b5118acd140754a979ab0003b8a6a4
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-05-25 16:49:36 -0700

    Fixed small error

--- flightController.py
+++ flightController.py
@@ -307,7 +307,7 @@ class LSM9DS0:
                 pitch *= 180 / PI
                 roll *= 180 / PI
 
-                print("Sample Count: " + str(self.testCounter) + " | dt: " + str(self.dt), end = "")
+                print("Sample Count: " + str(self.testCounter++) + " | dt: " + str(self.dt), end = "")
                 print(" | Yaw (No reference): " + str(yaw), end = "")
                 print(" | Pitch: " + str(pitch), end = "")
                 print(" | Roll: " + str(roll))
