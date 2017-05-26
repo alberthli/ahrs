29422ff79955ee044efacf116cc824bb7bee05f0
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-05-25 22:26:03 -0700

    Syntax

--- flightController.py
+++ flightController.py
@@ -58,18 +58,18 @@ class GPS:
                 report = session.next()
                 # Wait for a 'TPV' report and display the current time
                 # To see all report data, uncomment the line below
-                print report
-                
+                print(report)
+
                 if report['class'] == 'TPV':
                     if hasattr(report, 'time'):
-                        print report.time
+                        print(report.time)
             except KeyError:
                 pass
             except KeyboardInterrupt:
                 quit()
             except StopIteration:
                 session = None
-                print "GPSD has terminated"
+                print("GPSD has terminated")
 
 # Combined IMU class
 class LSM9DS0:
