047e8c78d14f43bf3126d153b6792b8ee54ebb01
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-05-29 14:51:11 -0700

    Syntax

--- test_SIMD.cpp
+++ test_SIMD.cpp
@@ -9,7 +9,7 @@ int main() {
 	cout << "Test Code" << "\n";
 	cout << "Test Vector Storing and Retreaving" << "\n";
 	float testArray[4] = {10, 20, 30, 40};
-	float32x4_t se_Vector = arm_neon::ld1q_f32(testArray);
+	float32x4_t se_Vector = vld1q_f32(testArray);
 	float retrieving[4];
 	vst1q_f32(retrieving, se_Vector);
 	cout << retrieving[0] << endl;
@@ -20,7 +20,7 @@ int main() {
 
 	cout << "Testing Divide" << endl;
 	float testDivider[4] = {0.1, 0.1, 0.1, 0.1};
-	float32x4_t div = arm_neon::ld1q_f32(testDivider);
+	float32x4_t div = vld1q_f32(testDivider);
 	se_Vector = vmulq_f32(se_Vector, div);
 	vst1q_f32(retrieving, se_Vector);
 	cout << retrieving[0] << endl;
