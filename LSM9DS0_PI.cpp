656ec2ab8dc92d7bafd52a4d051aef63387ebdb7
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-05-29 16:54:23 -0700

    syntax

--- LSM9DS0_PI.cpp
+++ LSM9DS0_PI.cpp
@@ -673,8 +673,8 @@ void LSM9DS0::madgwickFilterUpdate() {
 		bx = sqrt(hx * hx + hy * hy);
 		bz = hz;
 
-		printf("%li\n", currTime);
-		printf("%li\n", prevTime);
+		printf("%lli\n", currTime);
+		printf("%lli\n", prevTime);
 		printf("%f\n", dt);
 		/*
 		cout << "dt: " << dt << "\n";
