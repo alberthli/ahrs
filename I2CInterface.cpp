f1d585a00bd88c38983448d399358bd531b500c2
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-05-29 12:39:03 -0700

    New I2C Library Test

---
+++ I2CInterface.cpp
@@ -0,0 +1,63 @@
+/*
+ * File:   I2CInterface.cpp
+ * Author: Matthew
+ * https://github.com/matthew-t-watson/Picopter/blob/master/I2CInterface.h
+ *
+ * Created on 21 October 2012, 10:38
+ */
+
+#include "I2CInterface.h"
+
+I2CInterfaceClass I2CInterface;
+
+I2CInterfaceClass::I2CInterfaceClass()
+{
+    strcpy(filename_, "/dev/i2c-1");
+    openInterface();
+}
+
+I2CInterfaceClass::~I2CInterfaceClass()
+{
+    close(file_);
+}
+
+void I2CInterfaceClass::openInterface()
+{
+    pthread_mutex_lock (&I2Cmutex_);
+    if ((file_ = open(filename_, O_RDWR)) < 0)
+    {
+        std::cout << "Failed to open the i2c bus at path " << filename_ << std::endl;
+    }
+    pthread_mutex_unlock (&I2Cmutex_);
+}
+
+
+bool I2CInterfaceClass::writeRegister(unsigned char slaveAddress, unsigned char registerAddress, unsigned char* buf, unsigned char len)
+{
+    pthread_mutex_lock (&I2Cmutex_);
+    setSlaveAddress_(slaveAddress);
+    if (i2c_smbus_write_i2c_block_data(file_, registerAddress, len, buf) < 0)
+    {
+        std::cout << "I2C write failed writing to " << std::hex << slaveAddress << std::endl;
+    }
+    pthread_mutex_unlock (&I2Cmutex_);
+}
+
+bool I2CInterfaceClass::readRegister(unsigned char slaveAddress, unsigned char registerAddress, unsigned char* buf, unsigned char len)
+{
+    pthread_mutex_lock (&I2Cmutex_);
+    setSlaveAddress_(slaveAddress);
+    if (i2c_smbus_read_i2c_block_data(file_, registerAddress, len, buf) != len)
+    {
+        std::cout << "Incorrect number of bytes read" << std::endl;
+    }
+    pthread_mutex_unlock (&I2Cmutex_);
+}
+
+void I2CInterfaceClass::setSlaveAddress_(unsigned char address)
+{
+    if (ioctl(file_, I2C_SLAVE, address) < 0)
+    {
+        std::cout << "Failed to set I2C slave address at file descriptor " << file_ << " errno " << errno << std::endl;
+    }
+}
---
+++ I2CInterface.h
@@ -0,0 +1,43 @@
+/*
+ * File:   I2CInterface.h
+ * Author: Matthew
+ * https://github.com/matthew-t-watson/Picopter/blob/master/I2CInterface.h
+ *
+ * Created on 21 October 2012, 10:38
+ */
+
+#ifndef I2CINTERFACE_H
+#define	I2CINTERFACE_H
+
+
+#include <iostream>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+//#include <linux/i2c.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+class I2CInterfaceClass
+{
+public:
+    I2CInterfaceClass();
+    virtual ~I2CInterfaceClass();
+    bool writeRegister(unsigned char slaveAddress, unsigned char registerAddress, unsigned char* buf, unsigned char len);
+    bool readRegister(unsigned char slaveAddress, unsigned char registerAddress, unsigned char* buf, unsigned char len);
+    void openInterface();
+
+private:
+    void setSlaveAddress_(unsigned char address);
+    int file_;
+    char filename_[20];
+    pthread_mutex_t I2Cmutex_;
+};
+extern I2CInterfaceClass I2CInterface;
+
+#endif	/* I2CINTERFACE_H */
