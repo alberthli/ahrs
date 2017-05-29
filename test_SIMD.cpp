501d208f9b700db31ae01728322ed1821e6d8fbf
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-05-29 14:50:44 -0700

    Syntax

--- test_SIMD.cpp
+++ test_SIMD.cpp
@@ -9,24 +9,24 @@ int main() {
 	cout << "Test Code" << "\n";
 	cout << "Test Vector Storing and Retreaving" << "\n";
 	float testArray[4] = {10, 20, 30, 40};
-	float32x4_t se_Vector = ld1q_f32(testArray);
-	float retreaving[4];
-	vst1q_f32(retreaving, se_Vector);
-	cout << retreaving[0] << endl;
-	cout << retreaving[1] << endl;
-	cout << retreaving[2] << endl;
-	cout << retreaving[3] << endl;
+	float32x4_t se_Vector = arm_neon::ld1q_f32(testArray);
+	float retrieving[4];
+	vst1q_f32(retrieving, se_Vector);
+	cout << retrieving[0] << endl;
+	cout << retrieving[1] << endl;
+	cout << retrieving[2] << endl;
+	cout << retrieving[3] << endl;
 
 
 	cout << "Testing Divide" << endl;
-	float testDivider = {0.1, 0.1, 0.1, 0.1};
-	float32x4_t div = ld1q_f32(testDivider);
+	float testDivider[4] = {0.1, 0.1, 0.1, 0.1};
+	float32x4_t div = arm_neon::ld1q_f32(testDivider);
 	se_Vector = vmulq_f32(se_Vector, div);
-	vst1q_f32(retreaving, se_Vector);
-	cout << retreaving[0] << endl;
-	cout << retreaving[1] << endl;
-	cout << retreaving[2] << endl;
-	cout << retreaving[3] << endl;
+	vst1q_f32(retrieving, se_Vector);
+	cout << retrieving[0] << endl;
+	cout << retrieving[1] << endl;
+	cout << retrieving[2] << endl;
+	cout << retrieving[3] << endl;
 
 	return 0;
 
