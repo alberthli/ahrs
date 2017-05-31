493dcce8fe28e1ed64365e5d06fe4486e441b9c8
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-05-31 00:19:06 -0700

    Testing basic GPS read

--- GPS_PI.cpp
+++ GPS_PI.cpp
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
+#include "serial"
 #include <string.h>
 #include <thread>
 #include <chrono>
@@ -23,41 +20,18 @@ GPS::~GPS() {
 }
 
 void GPS::initialize() {
-	// FROM: http://www.raspberry-projects.com/pi/programming-in-c/uart-serial-port/using-the-uart
-	// Initializing the serial port
-	uart_filestream = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
+	int baudrate = 9600;
+	serial::bytesize_t databits = serial::eightbits;
+	serial::parity_t parity = serial::parity_none;
+	serial::stopbits_t stopbits = serial::stopbits_one;
+	serial::flowcontrol_t flowcontrol = serial::flowcontrol_none;
+	Serial* serialObj = new serial::Serial("/dev/ttyS0", baudrate, serial::Timeout(),
+	                      databits, parity, stopbits, flowcontrol);
 
-	if(uart_filestream == -1) {
-		printf("Error - Can't open serial port.\n");
+	while(true) {
+		std::string rawData = serialObj->readline();
+    	std::cout << rawData << std::endl;
 	}
-
-	// Configuring the UART
-	// Flags
-	// Baud rate:- B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800, B500000, B576000, B921600, B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000
-	// CSIZE:- CS5, CS6, CS7, CS8
-	// CLOCAL - Ignore modem status lines
-	// CREAD - Enable receiver
-	// IGNPAR = Ignore characters with parity errors
-	// ICRNL - Map CR to NL on input (Use for ASCII comms where you want to auto correct end of line characters - don't use for binary comms!)
-	// PARENB - Parity enable
-	// PARODD - Odd parity (else even)
-	struct termios options;
-	tcgetattr(uart_filestream, &options);
-	options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;
-	options.c_iflag = IGNPAR;
-	options.c_oflag = 0;
-	options.c_lflag = 0;
-	tcflush(uart_filestream, TCIFLUSH);
-	tcsetattr(uart_filestream, TCSANOW, &options);
-
-	unsigned char update_10_hz_code[17] = {'$', 'P', 'M', 'T', 'K', '2', '2', '0', ',', '1', '0', '0', '*', '2', 'F', '\r', '\n'};
-	unsigned char baudrate_115200_code[20] = {'$', 'P', 'M', 'T', 'K', '2', '5', '1', ',', '1', '1', '5', '2', '0', '0', '*', '1', 'F', '\r', '\n'};
-
-	write(uart_filestream, baudrate_115200_code, strlen(baudrate_115200_code));
-	std::this_thread::sleep_for(std::chrono::seconds(1));
-
-	write(uart_filestream, update_10_hz_code, strlen(update_10_hz_code));
-	std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void GPS::startGPS() {
@@ -66,6 +40,11 @@ void GPS::startGPS() {
 
 void GPS::printRawData() {
 	while(true) {
-		
+
 	}
+}
+
+int main() {
+	GPS gps = GPS();
+	gps.initialize();
 }
