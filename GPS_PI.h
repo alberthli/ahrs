e8526b9676e92b2960d44854b8a00b1841f160ed
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-06-01 00:38:29 -0700

    Syntax?

--- GPS_PI.h
+++ GPS_PI.h
@@ -14,7 +14,14 @@ public:
 	void printRawData();
 	std::vector<std::string> split(const std::string &s, char delim);
 	template<typename Out>
-	void split(const std::string &s, char delim, Out result);
+	void split(const std::string &s, char delim, Out result) {
+	    std::stringstream ss;
+	    ss.str(s);
+	    std::string item;
+	    while (std::getline(ss, item, delim)) {
+	        *(result++) = item;
+	    }
+	}
 
 private:
 	double lat;
