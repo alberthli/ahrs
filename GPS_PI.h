bc1f3e652a74618032611e0e1b502574b664f963
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-06-01 00:46:03 -0700

    Testing syntax

--- GPS_PI.h
+++ GPS_PI.h
@@ -24,4 +24,14 @@ private:
 	int uart_filestream;
 };
 
+template<typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+
 #endif
