3fb2ce5b579e8eb3d4147261c3f6da9dfdee8bfa
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-06-01 22:33:55 -0700

    try/catch

--- GPS_PI.cpp
+++ GPS_PI.cpp
@@ -38,90 +38,95 @@ void GPS::initialize() {
 }
 
 void GPS::startGPS() {
-	while(true) {
-		vector<string> lineData = splitString(serialInterface.readLine(), ',');
+	try {
+		while(true) {
+			vector<string> lineData = splitString(serialInterface.readLine(), ',');
 
-		if(!lineData.at(0).compare("$GPRMC")) {
+			if(!lineData.at(0).compare("$GPRMC")) {
 
-			// Getting latitude
-			string latString = lineData.at(3);
+				// Getting latitude
+				string latString = lineData.at(3);
 
-			if(latString.length() > 0) {
-				float lat1 = ::atof(latString.substr(0, 2).c_str());
-				float lat2 = ::atof(latString.substr(2).c_str()) / 60.0f;
+				if(latString.length() > 0) {
+					float lat1 = ::atof(latString.substr(0, 2).c_str());
+					float lat2 = ::atof(latString.substr(2).c_str()) / 60.0f;
 
-				lat = lat1 + lat2;
+					lat = lat1 + lat2;
 
-				if(!lineData.at(4).compare("S")) {
-					lat *= -1;
+					if(!lineData.at(4).compare("S")) {
+						lat *= -1;
+					}
+
+				} else {
+					continue;
 				}
 
-			} else {
-				continue;
-			}
+				// Getting longitude
+				string lonString = lineData.at(5);
 
-			// Getting longitude
-			string lonString = lineData.at(5);
+				if(latString.length() > 0) {
+					float lon1 = ::atof(lonString.substr(0, 3).c_str());
+					float lon2 = ::atof(lonString.substr(3).c_str()) / 60.0f;
 
-			if(latString.length() > 0) {
-				float lon1 = ::atof(lonString.substr(0, 3).c_str());
-				float lon2 = ::atof(lonString.substr(3).c_str()) / 60.0f;
+					lon = lon1 + lon2;
 
-				lon = lon1 + lon2;
+					if(!lineData.at(6).compare("W")) {
+						lon *= -1;
+					}
 
-				if(!lineData.at(6).compare("W")) {
-					lon *= -1;
+				} else {
+					continue;
 				}
 
+				// Getting speed
+				if(lineData.at(7).length() > 0) {
+					speed = ::atof(lineData.at(7).c_str()) * 0.51444444f;
+				} else {
+					continue;
+				}
+
+				// Getting course
+				if(lineData.at(8).length() > 0) {
+					cmg = ::atof(lineData.at(8).c_str());
+				} else {
+					continue;
+				}
+
+			} else if(!lineData.at(0).compare("$GPGGA")) {
+
+				// Getting number of satellites
+				if(lineData.at(7).length() > 0) {
+					numSats = ::stoi(lineData.at(7).c_str());
+				} else {
+					continue;
+				}
+
+			} else if(!lineData.at(0).compare("$GPGSA")) {
+
+				// Getting the horizontal dilution of precision
+				if(lineData.at(16).length() > 0) {
+					hdop = ::atof(lineData.at(16).c_str());
+				} else {
+					continue;
+				}
+
+			} else if(!lineData.at(0).compare("$GPVTG")) {
+
 			} else {
 				continue;
 			}
 
-			// Getting speed
-			if(lineData.at(7).length() > 0) {
-				speed = ::atof(lineData.at(7).c_str()) * 0.51444444f;
-			} else {
-				continue;
-			}
+			printf("lat: %f\n", lat);
+			printf("lon: %f\n", lon);
+			printf("speed: %f\n", speed);
+			printf("cmg: %f\n", cmg);
+			printf("numSats: %i\n", numSats);
+			printf("hdop: %f\n\n", hdop);
 
-			// Getting course
-			if(lineData.at(8).length() > 0) {
-				cmg = ::atof(lineData.at(8).c_str());
-			} else {
-				continue;
-			}
-
-		} else if(!lineData.at(0).compare("$GPGGA")) {
-
-			// Getting number of satellites
-			if(lineData.at(7).length() > 0) {
-				numSats = ::stoi(lineData.at(7).c_str());
-			} else {
-				continue;
-			}
-
-		} else if(!lineData.at(0).compare("$GPGSA")) {
-
-			// Getting the horizontal dilution of precision
-			if(lineData.at(16).length() > 0) {
-				hdop = ::atof(lineData.at(16).c_str());
-			} else {
-				continue;
-			}
-
-		} else if(!lineData.at(0).compare("$GPVTG")) {
-
-		} else {
-			continue;
 		}
 
-		printf("lat: %f\n", lat);
-		printf("lon: %f\n", lon);
-		printf("speed: %f\n", speed);
-		printf("cmg: %f\n", cmg);
-		printf("numSats: %i\n", numSats);
-		printf("hdop: %f\n\n", hdop);
-
+	} catch(std::invalid_argument) {
+		startGPS();
 	}
 }
 
