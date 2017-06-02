8e85f785229cb3724a0d25f96265ff586990b856
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-06-01 22:27:04 -0700

    compile errors

--- GPS_PI.cpp
+++ GPS_PI.cpp
@@ -13,6 +13,8 @@
 #include "GPS_PI.h"
 #include "SerialInterface.h"
 
+using namespace std;
+
 GPS::GPS() {
 	lat = 0.0;
 	lon = 0.0;
@@ -28,29 +30,29 @@ GPS::~GPS() {
 
 void GPS::initialize() {
 	serialInterface.writeString(BAUDRATE_115200_CODE);
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+	this_thread::sleep_for(chrono::seconds(1));
 	serialInterface.changeBaudrate(115200);
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+	this_thread::sleep_for(chrono::seconds(1));
 	serialInterface.writeString(UPDATE_10HZ_CODE);
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+	this_thread::sleep_for(chrono::seconds(1));
 }
 
 void GPS::startGPS() {
 	while(true) {
-		std::vector<std::string> lineData = splitString(serialInterface.readLine(), ',');
+		vector<string> lineData = splitString(serialInterface.readLine(), ',');
 
 		if(!lineData.at(0).compare("$GPRMC")) {
 
 			// Getting latitude
-			std::vector<std::string> latString = lineData.at(3);
+			string latString = lineData.at(3);
 
 			if(latString.length() > 0) {
-				float lat1 = ::atof(std::string::substr(0, 3).c_str());
-				float lat2 = ::atof(std::string::substr(2).c_str()) / 60.0f;
+				float lat1 = ::atof(latString.substr(0, 3).c_str());
+				float lat2 = ::atof(latString.substr(2).c_str()) / 60.0f;
 
 				lat = lat1 + lat2;
 
-				if(!lineData.at(4)compare("S")) {
+				if(!lineData.at(4).compare("S")) {
 					lat *= -1;
 				}
 
@@ -59,15 +61,15 @@ void GPS::startGPS() {
 			}
 
 			// Getting longitude
-			std::vector<std::string> lonString = lineData.at(5);
+			string lonString = lineData.at(5);
 
 			if(latString.length() > 0) {
-				float lon1 = ::atof(std::string::substr(0, 4).c_str());
-				float lon2 = ::atof(std::string::substr(3).c_str()) / 60.0f;
+				float lon1 = ::atof(lonString.substr(0, 4).c_str());
+				float lon2 = ::atof(lonString.substr(3).c_str()) / 60.0f;
 
 				lon = lon1 + lon2;
 
-				if(!lineData.at(6)compare("W")) {
+				if(!lineData.at(6).compare("W")) {
 					lon *= -1;
 				}
 
@@ -117,15 +119,15 @@ void GPS::startGPS() {
 
 void GPS::printRawData() {
 	while(true) {
-		// std::cout << serialInterface.readLine().c_str()[0] << "\n";
-		std::vector<std::string> lineData = splitString(serialInterface.readLine(), ',');
-		std::cout << lineData.at(0) << "\n";
+		// cout << serialInterface.readLine().c_str()[0] << "\n";
+		vector<string> lineData = splitString(serialInterface.readLine(), ',');
+		cout << lineData.at(0) << "\n";
 	}
 }
 
-std::vector<std::string> GPS::splitString(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
+vector<string> GPS::splitString(const string &s, char delim) {
+    vector<string> elems;
+    split(s, delim, back_inserter(elems));
     return elems;
 }
 
