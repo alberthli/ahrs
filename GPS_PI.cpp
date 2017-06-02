adb0593700304b173fe89321ae8f9436aa3ee47f
Albert Li <Albert@Alberts-MacBook-Pro.local>
2017-06-01 22:22:26 -0700

    First pass at GPS sentence parsing

--- GPS_PI.cpp
+++ GPS_PI.cpp
@@ -36,7 +36,83 @@ void GPS::initialize() {
 }
 
 void GPS::startGPS() {
+	while(true) {
+		std::vector<std::string> lineData = splitString(serialInterface.readLine(), ',');
 
+		if(!lineData.at(0).compare("$GPRMC")) {
+
+			// Getting latitude
+			std::vector<std::string> latString = lineData.at(3);
+
+			if(latString.length() > 0) {
+				float lat1 = ::atof(std::string::substr(0, 3).c_str());
+				float lat2 = ::atof(std::string::substr(2).c_str()) / 60.0f;
+
+				lat = lat1 + lat2;
+
+				if(!lineData.at(4)compare("S")) {
+					lat *= -1;
+				}
+
+			} else {
+				continue;
+			}
+
+			// Getting longitude
+			std::vector<std::string> lonString = lineData.at(5);
+
+			if(latString.length() > 0) {
+				float lon1 = ::atof(std::string::substr(0, 4).c_str());
+				float lon2 = ::atof(std::string::substr(3).c_str()) / 60.0f;
+
+				lon = lon1 + lon2;
+
+				if(!lineData.at(6)compare("W")) {
+					lon *= -1;
+				}
+
+			} else {
+				continue;
+			}
+
+			// Getting speed
+			if(lineData.at(7).length() > 0) {
+				speed = ::atof(lineData.at(7).c_str()) * 0.51444444f;
+			} else {
+				continue;
+			}
+
+			// Getting course
+			if(lineData.at(8).length() > 0) {
+				speed = ::atof(lineData.at(8).c_str());
+			} else {
+				continue;
+			}
+
+		} else if(!lineData.at(0).compare("$GPGGA")) {
+
+			// Getting number of satellites
+			if(lineData.at(7).length() > 0) {
+				numSats = ::stoi(lineData.at(7).c_str());
+			} else {
+				continue;
+			}
+
+		} else if(!lineData.at(0).compare("$GPGSA")) {
+
+			// Getting the horizontal dilution of precision
+			if(lineData.at(16).length() > 0) {
+				hdop = ::atof(lineData.at(16).c_str());
+			} else {
+				continue;
+			}
+
+		} else if(!lineData.at(0).compare("$GPVTG")) {
+
+		} else {
+			continue;
+		}
+	}
 }
 
 void GPS::printRawData() {
